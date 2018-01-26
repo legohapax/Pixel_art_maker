a722532a26a869396e84d3714f1a4179cc14e331
Diatka <vojtech.diatka@diatka-v-mac.local>
2018-01-26 21:45:50 +0100

    Finally dynamically created table is seeing the light

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 function makeGrid() {
-    
+    var table = $('<table></table>');
+    var row = $('<tr></tr>');
+    var cell = $('<td></td>');
+    var n = $("<td>dd</td>");
+    var lines = 10;
+    var cells = 10;
+
+   
+    for (var i = 0; i < lines; i++) {
+        var cell = "<tr>";
+        for (var y = 0; y < cells; y++) {
+            cell +="<td></td>"
+            //create a row here
+           
+        }
+        //append the row to the table
+        cell += "</tr>"
+        $( table ).append(cell);
     
     }
+    table.css("color","red")
 
-$( '#create' ).click(makeGrid)
+    $( "body" ).append(table)
+
+}
+
+$('#create').click(makeGrid);
