a727870eee6add4258931b7597539b4b70848079
Diatka <vojtech.diatka@diatka-v-mac.local>
2018-01-27 18:10:13 +0100

    for nikhil

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 function makeGrid() {
   var table = $("<table></table>");
-  var row = $("<tr></tr>");
-  var cell = $("<td></td>");
-  const lines = $("#width").val();
-  const cells = $("#height").val();
+  var lines = $( "#height" ).val();
+  var cells = $( "#width" ).val();
+  //var lines = 40;
+  //var cells = 40;
+
+  //alert(lines + " cells: " + cells);
 
   for (var i = 0; i < lines; i++) {
     var cell = "<tr>";
     for (var y = 0; y < cells; y++) {
       cell += "<td></td>";
-    }
-    //append the row to the table
+    };
     cell += "</tr>";
     table.append(cell);
-  }
+  };
   table.css("color", "red");
 
   $("body").append(table);
+  
 }
 
-$("#create").mousedown(makeGrid);
+
+
+$("#create").click(makeGrid);
+ 
+  
+
+
