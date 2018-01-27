3fedcdbe2cd0975fe27a9d0c8a2d8b5f81d34e24
Diatka <vojtech.diatka@diatka-v-mac.local>
2018-01-27 22:37:32 +0100

    second click on submit resets the color to white

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-function isInDocument(query){
+function isInDocument(query) {
   return document.querySelectorAll(query).length != 0;
 }
 
 function makeGrid() {
-  let table = $( "#table" );
-  let size = $( "#size_pick" );
-  let lines = $( "#height" ).val();
-  let cells = $( "#width" ).val();
-  size.on("submit", function(e) {
-    e.preventDefault();
-  });
+  if (isInDocument("td")) {
+      $( "td" ).css("background-color","white")
+  } else {
+    let table = $("#table");
+    let size = $("#size_pick");
+    let lines = $("#height").val();
+    let cells = $("#width").val();
+    size.on("submit", function(e) {
+      e.preventDefault();
+    });
 
-  for (var i = 0; i < lines; i++) {
-    var cell = "<tr>";
-    for (var y = 0; y < cells; y++) {
-      cell += "<td></td>";
+    for (var i = 0; i < lines; i++) {
+      var cell = "<tr>";
+      for (var y = 0; y < cells; y++) {
+        cell += "<td></td>";
+      }
+      cell += "</tr>";
+      table.append(cell);
     }
-    cell += "</tr>";
-    table.append(cell);
   }
-
   //$("body").append(table);
 }
 
-$( "#create" ).click(makeGrid);
+$("#create").click(makeGrid);
 
-$( 'table' ).on( 'click', 'td', function() {
-  let color = $( "#color_pick" ).val();
-  $( this ).css("background-color",color)
+$("table").on("click", "td", function() {
+  let color = $("#color_pick").val();
+  $(this).css("background-color", color);
 });
-
-
-
-
 
 // alert($.contains( "header", "h1"))
 
 // if (document.getElementByTagName('header')) {
 //   alert("prdel")
-//  } 
+//  }
